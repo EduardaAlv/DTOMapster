0e91cc114352827fac69df09b2eb8ffc78336140
Eduarda Alves <65419192+EduardaAlv@users.noreply.github.com>
2025-02-18 21:19:33 -0300

    Carregamento da biblioteca Mapster

---
+++ Models/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapsterDTOs.Models
+{
+    public class UserViewModel
+    {
+        public int IdUser { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Adress { get; set; }
+    }
+}
